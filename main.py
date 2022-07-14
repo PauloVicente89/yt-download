89242ca471b30fe2cf60a1b5ca45e836ea36296d
Paulo Viana <pauloviana089@gmail.com>
2022-07-14 14:40:52 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+
+# pegando url do video a ser baixado
+url = input(str("Url do vídeo: "))
+video = YouTube(url)
+
+# definindo a maior resoluçao do video
+stream = video.streams.get_highest_resolution()
+
+# baixando no diretorio escolhido
+stream.download(output_path = 'C:/Users/paulo/Desktop')
